e67c1e9edaa6ba2e70a93ca5be2e68c236e49dd7
CTII <clay.thompson13@gmail.com>
2024-01-11 16:15:31 -0500

    🔧🐍 Add url comments to urls in WebApp.
    - Test commit post pulling merged remote from Daavid first commit.

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
+# add register, login, profile, logout paths. 
+# If authenticated -> login, else register. 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls'))

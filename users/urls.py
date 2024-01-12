9ac89b191c73d11d90293b43d44f541f19ed34ba
CTII <clay.thompson13@gmail.com>
2024-01-12 16:51:10 -0500

    🐍🔗 Update user-related URLs and views
    - Define URL patterns for user-related pages
    - Map each URL to its corresponding view function
    - Include URLs for dashboard, inventory, invoices, analytics, register, and login
    - Improve organization and consistency in URL routing

--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('dashboard/', views.home),
-    path('inventory/', views.inventory),
-    path('invoices/', views.invoices),
-    path('analytics/', views.analytics),
+    path('dashboard/', views.home, name='dashboard'),
+    path('inventory/', views.inventory, name='inventory'),
+    path('invoices/', views.invoices, name='invoices'),
+    path('analytics/', views.analytics, name='analytics'),
+    path('register/', views.register, name='register'),
+    path('login/', views.loginPage, name='loginPage'),
    
 ]

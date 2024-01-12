b49a88d5f9671425e281e3dc9b01c4ccac75e9d9
CTII <clay.thompson13@gmail.com>
2024-01-12 16:44:41 -0500

    🐍🔧 Refactor user views and add registration functionality
    - Import necessary modules and forms for user-related views
    - Define views for 'home', 'inventory', 'invoices', and 'analytics'
    - Implement registration functionality with CreateUserForm
      - Initialize form, process POST request, and create user account
      - Display success message and redirect to login page upon successful registration
    - Add login functionality
      - Handle POST request, authenticate user, and redirect to dashboard on success
      - Provide feedback message for incorrect username or password

--- users/views.py
+++ users/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
@@ -15,3 +20,34 @@ def analytics(request):
     return HttpResponse('analytics')
 
 
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user )
+            return redirect('login')
+        
+
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
+
+
+def loginPage(request):
+    if request.method =='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.info(request,'username OR password is incorrect')
+            
+    context = {}
+    return render(request, 'users/login.html', context)

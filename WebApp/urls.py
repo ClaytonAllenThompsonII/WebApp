3b43acbe0ba4f94cf9218eaee7112f3e6da0e5c6
CTII <clay.thompson13@gmail.com>
2024-01-12 16:52:03 -0500

    🐍🔧 Update static and media file settings in WebApp
    - Set STATIC_URL to '/static/' for serving static files
    - Set MEDIA_URL to '/images/' for serving media files
    - Add STATICFILES_DIR to include the 'static' directory in the project's base directory

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 # If authenticated -> login, else register. 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls'))
+    path('', include('users.urls')),
+    
    
 ]

a342a188d4b13d021ba703258278fc62ed33b465
CTII <clay.thompson13@gmail.com>
2024-01-13 23:13:53 -0500

    🐍🔄 Update Views in users in WebApp
    - Added `@login_required` decorator to protect views like `home`, `inventory`, `invoices`, and `analytics` from unauthorized access.
    - Modified the `register` and `loginPage` views to handle cases where the user is already authenticated.
    - Consolidated the logout view, `logoutUser`, and added the `@login_required` decorator.

--- users/views.py
+++ users/views.py
@@ -1,58 +1,69 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
-from .forms import CreateUserForm
-
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import CreateUserForm
+
 # Create your views here.
 
+@login_required(login_url='loginPage')
 def home(request):
     return render(request, 'users/dashboard.html')
 
+@login_required(login_url='loginPage')
 def inventory(request):
     return render(request, 'users/inventory.html')
 
+@login_required(login_url='loginPage')
 def invoices(request):
     return HttpResponse('invoices')
 
+@login_required(login_url='loginPage')
 def analytics(request):
     return HttpResponse('analytics')
 
 
 def register(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    else:
+        form = CreateUserForm()
 
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Account was created for ' + user )
-            return redirect('login')
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user )
+                return redirect('login')
         
 
-    context = {'form': form}
-    return render(request, 'users/register.html', context)
+        context = {'form': form}
+        return render(request, 'users/register.html', context)
 
 
 def loginPage(request):
-    if request.method =='POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    else:
+        if request.method =='POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            return redirect('dashboard')
-        else:
-            messages.info(request,'username OR password is incorrect')
+            if user is not None:
+                login(request, user)
+                return redirect('dashboard')
+            else:
+                messages.info(request,'username OR password is incorrect')
             
-    context = {}
-    return render(request, 'users/login.html', context)
+        context = {}
+        return render(request, 'users/login.html', context)
 
-
+@login_required(login_url='loginPage')
 def logoutUser(request):
     logout(request)
     return redirect('loginPage')

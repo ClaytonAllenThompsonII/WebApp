47814b9895e259794751872c9e512a371c21b633
CTII <clay.thompson13@gmail.com>
2024-01-13 23:15:10 -0500

    🐍🔄 Update URLs in users in WebApp.
    - Added paths for password reset functionality using Django's built-in views.
    - Commented the purpose of each password reset-related URL path.

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+
+from django.contrib.auth import views as auth_views
 from . import views
 
 
@@ -12,4 +14,36 @@ urlpatterns = [
     path('login/', views.loginPage, name='loginPage'),
     path('logout/', views.logoutUser, name='logout'),
    
+
+   path('reset_password/',
+    auth_views.PasswordResetView.as_view(template_name="users/password_reset.html"),
+    name="reset_password"),
+
+   path('reset_password_sent/',
+    auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_sent.html"),
+    name="password_reset_done"),
+
+   path('reset/<uibd64>/<token>/', 
+    auth_views.PasswordResetConfirmView.as_view(), 
+    name="password_reset_confirm"),
+
+   path('reset_password_complete/', 
+    auth_views.PasswordResetCompleteView.as_view(),
+    name="password_reset_complete"),
 ]
+
+
+"""
+Handles the initial request to initiate a password reset.
+
+- Displays the form for users to enter their email address.
+- Sends a password reset email if a matching email is found.
+- Redirects to the 'password_reset_done' view upon success.
+
+1- Submit email form                        //PasswordResetView.as_view()
+2- Email sent success message               //PasswordResetDoneView.as_view() 
+3- Link to password Rest form in email      //PasswordResetConfirmView.as_view()
+4- Password successfully changed message    //PasswordResetCompleteView.as_view()
+
+"""
+

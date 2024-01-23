0c9b8900f8587b4731949c0c90b961073c8f5da6
CTII <clay.thompson13@gmail.com>
2024-01-23 01:42:24 -0500

    🔧🐍 Fix AWSStorageBackend init in storage_backends.py In Web App
    - Initializing the AWS S3 and DynamoDB clients using environment variables for credentials.
    - Adding error handling to catch exceptions during client initialization.
    - Logging successful AWS client initialization.

--- inventory/storage_backends.py
+++ inventory/storage_backends.py
@@ -4,25 +4,38 @@ import uuid
 from botocore.config import Config
 from django.conf import settings
 import boto3
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class AWSStorageBackend:
     """Handles interactions with AWS S3 and DynamoDB for image storage and metadata management."""
     def __init__(self) -> None:
         """Initialize the S3 and DynamoDB clients using environment variables for credentials""" 
-        self.s3_client = boto3.client(
-            's3',
-            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-            region_name=settings.AWS_REGION,
-            config=Config(signature_version='s3v4')
-            )
-        
-        self.dynamodb_client = boto3.client('dynamodb')
+        try:
+            self.s3_client = boto3.client(
+                's3',
+                aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+                region_name=settings.AWS_REGION,
+                config=Config(signature_version='s3v4')
+                )
+            
+            self.dynamodb_client = boto3.client(
+                'dynamodb',
+                region_name=settings.AWS_REGION
+        )
 
-        #set bucket and table names from environment variables
-        self.bucket_name = os.environ['S3_BUCKET_NAME'] # user image upload bucket  
-        self.table_name = os.environ['DYNAMODB_TABLE_NAME'] # image label bucket
+            #set bucket and table names from environment variables
+            self.bucket_name = os.environ['S3_BUCKET_NAME'] # user image upload bucket  
+            self.table_name = os.environ['DYNAMODB_TABLE_NAME'] # image label bucket
+
+            logger.info("AWS clients for S3 and DynamoDB initialized successfully")
+        except Exception as e:
+            logger.error("Error initializing AWS clients: %s", e)
+            raise
+
 
     def upload_file(self, file):
         """Uploads a file to S3 and returns the generated filename"""
@@ -49,8 +62,10 @@ class AWSStorageBackend:
          Provides the actual data to be inserted into the item. It's a dictionary containing
           the attribute name and values from the new item. 
         """
+        print("Attempting to successfully upload to dynamoDB:", item_data, self.table_name)
         try:
             self.dynamodb_client.put_item(TableName=self.table_name, Item=item_data)
+            print("Item_data successfully uploaded to dynamoDB:", item_data, self.table_name)
         except Exception as e: 
             raise Exception(f'Error creating item in DynamopDB: {e}') from e
             #will add less generic exception handling eventually. 

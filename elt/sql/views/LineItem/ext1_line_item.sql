695ca0aa6a6e33f0d667286dc7dc41c34c4c70ea
CTII <clay.thompson13@gmail.com>
2024-04-01 00:10:15 -0400

    🔧💿 Refactored ext1_line_item view for efficient JSON data extraction
    - Use jsonb_array_elements to iterate through nested JSON arrays for ExpenseDocuments, LineItemGroups, and LineItems.
    - Utilize li_expense_fields(value) to access individual expense fields within each line item.
    - Explicitly casts li_expense_fields to jsonb before extracting textual data using ->>'Text' for better clarity and potential performance benefits.
    - Includes both the original JSON representation (type, value_detection, label_detection) and the extracted text (type_text, vd_text, ld_text) for potential future use cases.

--- elt/sql/views/LineItem/ext1_line_item.sql
+++ elt/sql/views/LineItem/ext1_line_item.sql
@@ -2,10 +2,24 @@ CREATE OR REPLACE VIEW ext1_line_item AS
 SELECT
     inp.id AS in_invoice_processing_id,
     inp.s3_object_key,
-    li_expense_fields.value AS line_item_expense_field,
-    inp.received_timestamp
+    
+    inp.received_timestamp,
+
+    (li_expense_fields->'Type')::jsonb->>'Text' AS type_text,  -- Cast to JSONB before extraction
+    li_expense_fields->>'Type' AS type,
+
+    (li_expense_fields->'ValueDetection')::jsonb->>'Text' AS vd_text,  -- Cast to JSONB before extraction
+    li_expense_fields->>'ValueDetection' AS value_detection,
+
+    (li_expense_fields->'LabelDetection')::jsonb->>'Text' AS ld_text,  -- Cast to JSONB before extraction
+    li_expense_fields->>'LabelDetection' AS label_detection,
+
+    li_expense_fields.value AS line_item_expense_field
 FROM
     in_invoice_processing inp,
     jsonb_array_elements(inp.textract_json->'ExpenseDocuments'->0->'LineItemGroups') AS lig(value),
     jsonb_array_elements(lig.value->'LineItems') AS li(value),
     jsonb_array_elements(li.value->'LineItemExpenseFields') AS li_expense_fields(value);
+
+    -- where (li_expense_fields->'Type')::jsonb->>'Text' = 'ITEM'
+    -- for All ITEMs (line item product descriptions)

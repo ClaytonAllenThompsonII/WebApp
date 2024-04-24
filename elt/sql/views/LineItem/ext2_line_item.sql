5d5776bc2fdbf8f33b2112fe9c4793d5aee6a0f3
CTII <clay.thompson13@gmail.com>
2024-04-24 02:06:35 -0400

    🔧💿 Refactor aggregated line item view in ELT in WebApp
    - ext2_line_item view aggregates data from the ext1_line_item view.
    - It transforms type_text values into distinct columns for each line_item_index.
    - The view includes derived fields such as 'product_code', 'item', 'unit_price', 'max_other', 'price', and 'expense_row'.
    - Grouping is performed by invoice processing IDs, S3 object keys, receipt timestamps, and line item indices to ensure coherent aggregation of data per item.
    - The view serves as a crucial component for downstream processes requiring organized and accessible data for analytics, reporting, or transformations.

--- elt/sql/views/LineItem/ext2_line_item.sql
+++ elt/sql/views/LineItem/ext2_line_item.sql
@@ -1,22 +1,37 @@
 CREATE OR REPLACE VIEW ext2_line_item AS
 
+SELECT 
+    in_invoice_processing_id,
+    s3_object_key,
+    received_timestamp,
+    line_item_index,
+    MAX(CASE WHEN type_text = 'PRODUCT_CODE' THEN vd_text ELSE NULL END) AS product_code,
+	MAX(CASE WHEN type_text = 'ITEM' THEN vd_text ELSE NULL END) AS item,
+    MAX(CASE WHEN type_text = 'UNIT_PRICE' THEN vd_text ELSE NULL END) AS unit_price,
+	MAX(CASE WHEN type_text = 'OTHER' THEN vd_text ELSE NULL END) AS max_other,
+    MAX(CASE WHEN type_text = 'PRICE' THEN vd_text ELSE NULL END) AS price,
+    
+	MAX(CASE WHEN type_text = 'EXPENSE_ROW' THEN vd_text ELSE NULL END) AS expense_row
+   
+    
+FROM 
+    ext1_line_item
+GROUP BY
+    in_invoice_processing_id, s3_object_key, received_timestamp, line_item_index
+ORDER BY
+    in_invoice_processing_id, line_item_index;
 
-SELECT
-    inp.id AS in_invoice_processing_id,
-    inp.s3_object_key,
-    li_expense_fields.value AS line_item_expense_field,
-    inp.received_timestamp,
-    MAX(CASE WHEN ((li_expense_fields->'Type')::jsonb->>'Text') = 'PRODUCT_CODE' THEN (li_expense_fields->'ValueDetection')::jsonb->>'Text' END) AS product_code,
-    MAX(CASE WHEN ((li_expense_fields->'Type')::jsonb->>'Text') = 'ITEM' THEN (li_expense_fields->'ValueDetection')::jsonb->>'Text' END) AS item,
-    MAX(CASE WHEN ((li_expense_fields->'Type')::jsonb->>'Text') = 'QUANTITY' THEN (li_expense_fields->'ValueDetection')::jsonb->>'Text' END) AS quantity,
-    MAX(CASE WHEN ((li_expense_fields->'Type')::jsonb->>'Text') = 'UNIT_PRICE' THEN (li_expense_fields->'ValueDetection')::jsonb->>'Text' END) AS unit_price,
-    MAX(CASE WHEN ((li_expense_fields->'Type')::jsonb->>'Text') = 'PRICE' THEN (li_expense_fields->'ValueDetection')::jsonb->>'Text' END) AS price
-FROM
-    in_invoice_processing inp,
-    jsonb_array_elements(inp.textract_json->'ExpenseDocuments'->0->'LineItemGroups') AS lig(value),
-    jsonb_array_elements(lig.value->'LineItems') AS li(value),
-    jsonb_array_elements(li.value->'LineItemExpenseFields') AS li_expense_fields(value)
-	
-	group by in_invoice_processing_id, s3_object_key, li_expense_fields.value, li;
 
+
+COMMENT ON VIEW ext2_line_item IS $$
+This view aggregates data from the 'ext1_line_item' view by transforming 'type_text' values into distinct columns for each 'line_item_index'. Each line item index groups related attributes from the source document, facilitating structured data analysis and reporting. The view specifically aggregates:
+- 'product_code': Derived from 'PRODUCT_CODE' entries in 'type_text'.
+- 'item': Extracted from 'ITEM' type_text entries.
+- 'unit_price': Pulled from 'UNIT_PRICE' type_text entries.
+- 'max_other': A maximum (or latest if dates/times involved) 'vd_text' value from entries classified as 'OTHER' in 'type_text'. This helps consolidate various 'OTHER' descriptions into a single column.
+- 'price': Represents the maximum 'vd_text' extracted from 'PRICE' type_text entries.
+- 'expense_row': Captures expense details from 'EXPENSE_ROW' type_text, providing a comprehensive expense summary.
+
+Grouping is performed by invoice processing IDs, S3 object keys, receipt timestamps, and line item indices to ensure coherent aggregation of data per item. The view is crucial for downstream processes that require organized and accessible data for further analytics, reporting, or transformations.
+$$;
 

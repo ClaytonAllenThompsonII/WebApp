fb5252a45adc82a6b350110a1b0ab7f20c5e6de3
CTII <clay.thompson13@gmail.com>
2024-04-30 00:35:17 -0400

    🔧💿 Refactor JSONB field extraction and join logic in ext1_vendor view
    - Update JSONB field extraction and join strategy in the vendor query, incorporating JOIN LATERAL and COALESCE for improved efficiency and flexibility.
    - The previous version utilized explicit joins with the JOIN keyword, resulting in separate scans of the tables and potentially unnecessary processing.
    - By leveraging JOIN LATERAL in the revised query, we optimize performance by executing the lateral join for each row returned by the outer query, reducing the need for repeated table scans and improving overall query efficiency.

--- elt/sql/views/Vendors/ext1_vendor.sql
+++ elt/sql/views/Vendors/ext1_vendor.sql
@@ -2,30 +2,26 @@ CREATE OR REPLACE VIEW ext1_vendor AS
 
 
 SELECT
-  inp.id AS processing_id,
-  inp.s3_object_key,
-  (sf->'Type')::jsonb->>'Text' AS type_text,  -- Cast to JSONB before extraction
-  sf->>'Type' AS type,
+    inp.id AS processing_id,
+    inp.s3_object_key,
+    sf.idx AS field_index,
+    sf.value ->> 'Type' AS field_type,
+    (sf.value -> 'Type' ->> 'Text') AS type_text,
+    (sf.value -> 'ValueDetection' ->> 'Text') AS value_text,
+    (sf.value -> 'LabelDetection' ->> 'Text') AS label_text,
+    COALESCE(gp.value ->> 'Types', '[]') AS group_types,
+    CASE
+        WHEN 'VENDOR' = ANY(ARRAY(SELECT jsonb_array_elements_text(COALESCE(gp.value -> 'Types', '[]'::jsonb)))) THEN TRUE
+        ELSE FALSE
+    END AS is_vendor,
+    CASE
+        WHEN 'VENDOR_REMIT_TO' = ANY(ARRAY(SELECT jsonb_array_elements_text(COALESCE(gp.value -> 'Types', '[]'::jsonb)))) THEN TRUE
+        ELSE FALSE
+    END AS is_vendor_remit_to
+FROM
+    in_invoice_processing inp
+JOIN LATERAL
+    jsonb_array_elements(inp.textract_json -> 'ExpenseDocuments' -> 0 -> 'SummaryFields') WITH ORDINALITY AS sf(value, idx) ON true
+LEFT JOIN LATERAL
+    jsonb_array_elements(sf.value -> 'GroupProperties') AS gp(value) ON true;
 
-  (gp->'Types')::jsonb->>0 as group_text,
-  gp->>'Types' AS group_type,
-
-  (sf->'ValueDetection')::jsonb->>'Text' AS vd_text,  -- Cast to JSONB before extraction
-  sf->>'ValueDetection' AS value_detection,
-
-  (sf->'LabelDetection')::jsonb->>'Text' AS ld_text,  -- Cast to JSONB before extraction
-  sf->>'LabelDetection' AS label_detection,
-
-CASE
-    WHEN (gp->'Types')::jsonb->>0 = 'VENDOR' THEN TRUE
-    ELSE FALSE
-END AS is_vendor,
-
-CASE
-    WHEN (gp->'Types')::jsonb->>0 = 'VENDOR_REMIT_TO'  THEN TRUE
-    ELSE FALSE
-END AS is_vendor_remit_to
-
-FROM in_invoice_processing inp,
-  LATERAL jsonb_array_elements(inp.textract_json->'ExpenseDocuments'->0->'SummaryFields') AS sf,
-  LATERAL jsonb_array_elements(sf->'GroupProperties') AS gp;

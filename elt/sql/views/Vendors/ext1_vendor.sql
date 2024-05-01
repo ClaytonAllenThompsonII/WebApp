72883a4df146ef1aa79937902eccce5826671b1b
CTII <clay.thompson13@gmail.com>
2024-05-01 00:47:26 -0400

    🔧💿 Add address role classification to ext1_vendor view in ELT in WebApp
    -This commit introduces the address_role field to the ext1_vendor view, which categorizes addresses based on their role (e.g., sold_to, ship_to, bill_to, remit_to, vendor).
    - The address_role field is populated based on the presence of specific types in the group_types JSONB array, assigning default value 'unclassified' for records with empty group sets.
    - Remove old vendor and vendor remit to case logic.

--- elt/sql/views/Vendors/ext1_vendor.sql
+++ elt/sql/views/Vendors/ext1_vendor.sql
@@ -10,18 +10,22 @@ SELECT
     (sf.value -> 'ValueDetection' ->> 'Text') AS value_text,
     (sf.value -> 'LabelDetection' ->> 'Text') AS label_text,
     COALESCE(gp.value ->> 'Types', '[]') AS group_types,
+    sf.value -> 'ValueDetection' ->> 'Text' AS address_text,
+  COALESCE(
     CASE
-        WHEN 'VENDOR' = ANY(ARRAY(SELECT jsonb_array_elements_text(COALESCE(gp.value -> 'Types', '[]'::jsonb)))) THEN TRUE
-        ELSE FALSE
-    END AS is_vendor,
-    CASE
-        WHEN 'VENDOR_REMIT_TO' = ANY(ARRAY(SELECT jsonb_array_elements_text(COALESCE(gp.value -> 'Types', '[]'::jsonb)))) THEN TRUE
-        ELSE FALSE
-    END AS is_vendor_remit_to
+        WHEN 'RECEIVER_SOLD_TO' = ANY (SELECT jsonb_array_elements_text(gp.value -> 'Types')) THEN 'sold_to'
+        WHEN 'RECEIVER_SHIP_TO' = ANY (SELECT jsonb_array_elements_text(gp.value -> 'Types')) THEN 'ship_to'
+		WHEN 'RECEIVER_BILL_TO' = ANY (SELECT jsonb_array_elements_text(gp.value -> 'Types')) THEN 'bill_to'
+        WHEN 'VENDOR_REMIT_TO' = ANY (SELECT jsonb_array_elements_text(gp.value -> 'Types')) THEN 'remit_to'
+        WHEN 'VENDOR' = ANY (SELECT jsonb_array_elements_text(gp.value -> 'Types')) THEN 'vendor'
+        -- Add other cases as necessary for BILL_TO, SUPPLIER, etc.
+    END, 
+    'unclassified'  -- Defautl Value for records with empty group sets
+    ) AS address_role
 FROM
     in_invoice_processing inp
 JOIN LATERAL
-    jsonb_array_elements(inp.textract_json -> 'ExpenseDocuments' -> 0 -> 'SummaryFields') WITH ORDINALITY AS sf(value, idx) ON true
+    jsonb_array_elements(inp.textract_json -> 'ExpenseDocuments' -> 0 -> 'SummaryFields') WITH ORDINALITY AS sf(value, idx) ON TRUE
 LEFT JOIN LATERAL
-    jsonb_array_elements(sf.value -> 'GroupProperties') AS gp(value) ON true;
+    jsonb_array_elements(sf.value -> 'GroupProperties') AS gp(value) ON TRUE
 

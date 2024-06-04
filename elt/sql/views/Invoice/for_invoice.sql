8d2f996b1c76ee6d70bed33dfc117aaf572d1070
CTII <clay.thompson13@gmail.com>
2024-06-04 17:28:27 -0400

    ✨💿 Update for_invoice view used for invoice upsert script
    - Format and clean data extracted from ext2_invoice.sql to prepare for upsertion.
    - Use COALESCE and REGEXP_REPLACE to handle various formats and standardize account numbers, vendor names, and dates.
    - Convert dates to 'MM/DD/YYYY' format and store them as DATE.
    - Extract and clean invoice receipt IDs, removing hyphens and whitespace.
    - Standardize total amounts, removing non-numeric characters and converting to DECIMAL.

--- elt/sql/views/Invoice/for_invoice.sql
+++ elt/sql/views/Invoice/for_invoice.sql
@@ -1,38 +1,95 @@
 CREATE OR REPLACE VIEW for_invoice AS
+SELECT 
 
-SELECT
-    -- Identifiers and Metadata
-    in_invoice_processing_id,
-    s3_object_key,
-    received_timestamp,
-
-    -- Invoice Details
-    invoice_receipt_id,
-    COALESCE(customer_number, account_number) AS customer_account_number,
-    taxpayer_id,
-    INITCAP(vendor_name) AS vendor_name,
-    -- Assuming vendor_id will be populated later
-    NULL::INT AS vendor_id,
-
-    -- Dates: Removing day names and parsing to DATE
-    TO_DATE(REGEXP_REPLACE(due_date, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY') AS due_date,
+ in_invoice_processing_id,
+ s3_object_key,
+ received_timestamp as upload_date,
+ -- Customer Account Number
+ COALESCE(account_number_label, account_number_hash, customer_number_label, customer_number_customer) as account_number,
+ -- Vendor Name
+ INITCAP(vendor_name) AS vendor_name,
+  -- Delivery date (converting to MM/DD/YYYY format)
+    TO_CHAR(
+        COALESCE(
+            TO_DATE(REGEXP_REPLACE(delivery_date_label, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY'),
+            TO_DATE(REGEXP_REPLACE(delivery_date_time, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY')
+        ), 
+        'MM/DD/YYYY'
+    ) AS delivery_date,
+	
+	 -- Invoice receipt date (converting to MM/DD/YYYY format)
+    TO_CHAR(
+        COALESCE(
+            TO_DATE(REGEXP_REPLACE(invoice_receipt_date, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY'),
+            TO_DATE(REGEXP_REPLACE(invoice_receipt_date_upper, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY'),
+            TO_DATE(REGEXP_REPLACE(invoice_receipt_date_invoice, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY'),
+            TO_DATE(REGEXP_REPLACE(invoice_receipt_date_label, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY'),
+            TO_DATE(REGEXP_REPLACE(invoice_receipt_date_delv_period, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY'),
+            TO_DATE(REGEXP_REPLACE(invoice_receipt_date_delv, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY'),
+			TO_DATE(REGEXP_REPLACE(delivery_date_label, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY'),
+            TO_DATE(REGEXP_REPLACE(delivery_date_time, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY')
+        ), 
+        'MM/DD/YYYY'
+    ) AS invoice_date,
+	
+	-- Due date (converting to MM/DD/YYYY format)
+    TO_CHAR(
+        COALESCE(
+            TO_DATE(REGEXP_REPLACE(due_date, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY'),
+            TO_DATE(REGEXP_REPLACE(due_date_due, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY'),
+            TO_DATE(REGEXP_REPLACE(due_date_label, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY'),
+            TO_DATE(REGEXP_REPLACE(due_date_payment, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY')
+        ), 
+        'MM/DD/YYYY'
+    ) AS due_date,
+	
+	-- Invoice receipt ID (removing hyphens and whitespace)
+    REGEXP_REPLACE(
+        COALESCE(
+            invoice_receipt_id_number,
+            invoice_receipt_id_invoice,
+            invoice_receipt_id_label,
+            invoice_receipt_id_number_upper
+        ), 
+        '[-\s]', '', 'g'
+    ) AS invoice_number,
+	
+	 -- Total information (removing non-numeric characters and converting to DECIMAL)
     COALESCE(
-        TO_DATE(REGEXP_REPLACE(invoice_receipt_date, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY'),
-        TO_DATE(REGEXP_REPLACE(delivery_date, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY')
-    ) AS invoice_receipt_date_plus,
+        REGEXP_REPLACE(total, '[^\d.-]', '', 'g')::DECIMAL,
+        REGEXP_REPLACE(total_pay_this_amount, '[^\d.-]', '', 'g')::DECIMAL,
+        REGEXP_REPLACE(total_invoice, '[^\d.-]', '', 'g')::DECIMAL,
+        REGEXP_REPLACE(total_totals, '[^\d.-]', '', 'g')::DECIMAL
+    ) AS total,
+	
+	
+	   -- Delivery date (storing as DATE)
+    COALESCE(
+        TO_DATE(REGEXP_REPLACE(delivery_date_label, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY')::DATE,
+        TO_DATE(REGEXP_REPLACE(delivery_date_time, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY')::DATE
+    ) AS delivery_date_date,
 
-    -- Purchase Order Number
-    po_number,
+    -- Invoice receipt date (storing as DATE)
+    COALESCE(
+        TO_DATE(REGEXP_REPLACE(invoice_receipt_date, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY')::DATE,
+        TO_DATE(REGEXP_REPLACE(invoice_receipt_date_upper, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY')::DATE,
+        TO_DATE(REGEXP_REPLACE(invoice_receipt_date_invoice, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY')::DATE,
+        TO_DATE(REGEXP_REPLACE(invoice_receipt_date_label, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY')::DATE,
+        TO_DATE(REGEXP_REPLACE(invoice_receipt_date_delv_period, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY')::DATE,
+        TO_DATE(REGEXP_REPLACE(invoice_receipt_date_delv, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY')::DATE,
+        TO_DATE(REGEXP_REPLACE(delivery_date_label, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY')::DATE,
+        TO_DATE(REGEXP_REPLACE(delivery_date_time, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY')::DATE
+    ) AS invoice_date_date,
 
-    -- Payment Terms
-    INITCAP(COALESCE(payment_terms_colon, terms, terms_upper)) AS payment_terms,
+    -- Due date (storing as DATE)
+    COALESCE(
+        TO_DATE(REGEXP_REPLACE(due_date, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY')::DATE,
+        TO_DATE(REGEXP_REPLACE(due_date_due, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY')::DATE,
+        TO_DATE(REGEXP_REPLACE(due_date_label, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY')::DATE,
+        TO_DATE(REGEXP_REPLACE(due_date_payment, '^[A-Za-z]+, ', ''), 'MM/DD/YYYY')::DATE
+    ) AS due_date_date
+	
 
-    -- Financial Data: Remove non-numeric characters and cast to DECIMAL
-    REGEXP_REPLACE(COALESCE(invoice_total, Total, invoice_total_2, pay_this_amount, due_amount), '[^\d.-]', '', 'g')::DECIMAL AS total,
-    REGEXP_REPLACE(total_gross_amount, '[^\d.-]', '', 'g')::DECIMAL AS subtotal,
-    REGEXP_REPLACE(tax, '[^\d.-]', '', 'g')::DECIMAL AS tax,
-    REGEXP_REPLACE(service_charge, '[^\d.-]', '', 'g')::DECIMAL AS service_charge,
-    REGEXP_REPLACE(discount, '[^\d.-]', '', 'g')::DECIMAL AS discount
 
-FROM
-    ext2_invoice;
+ FROM ext2_invoice
+ 

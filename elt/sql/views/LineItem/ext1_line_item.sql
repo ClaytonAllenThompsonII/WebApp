ee1f7b66fda9e254bd0fcbd135bb016ea9db757e
CTII <clay.thompson13@gmail.com>
2024-06-30 22:17:08 -0400

    🔧💿Refactor ext1_line_item View to Optimize Invoice Receipt ID Extraction
    - Refactor the `ext1_line_item` view to improve extraction of invoice receipt IDs.
    - Introduce a CTE `preferred_invoice_id` to streamline the extraction of invoice receipt IDs using `MAX` function for better performance and readability.
    - Update the main query to join `preferred_invoice_id` for efficient invoice receipt ID association.
    - The new structure enhances data processing efficiency and ensures consistent handling of invoice receipt IDs across all line items.

--- elt/sql/views/LineItem/ext1_line_item.sql
+++ elt/sql/views/LineItem/ext1_line_item.sql
@@ -1,4 +1,16 @@
 CREATE OR REPLACE VIEW ext1_line_item AS
+WITH preferred_invoice_id AS (
+    SELECT 
+        inp.id AS in_invoice_processing_id,
+        MAX(CASE WHEN summary_field -> 'Type' ->> 'Text' = 'INVOICE_RECEIPT_ID' THEN summary_field -> 'ValueDetection' ->> 'Text' END) AS invoice_receipt_id
+    FROM 
+        in_invoice_processing inp
+    JOIN LATERAL
+        jsonb_array_elements(inp.textract_json -> 'ExpenseDocuments') AS ed(value) ON true
+    JOIN LATERAL
+        jsonb_array_elements(ed.value -> 'SummaryFields') AS summary_field ON true
+    GROUP BY inp.id
+)
 SELECT 
     inp.id AS in_invoice_processing_id,
     inp.s3_object_key,
@@ -8,7 +20,7 @@ SELECT
     (li_expense_fields.value -> 'Type'::text) ->> 'Text'::text AS type_text,
     REPLACE((li_expense_fields.value -> 'ValueDetection'::text) ->> 'Text'::text, E'\n', ' ') AS vd_text,  -- Replace newlines with spaces
     REPLACE((li_expense_fields.value -> 'LabelDetection' ->> 'Text'::text), E'\n', ' ') AS ld_text,  -- Replace newlines with spaces
-    sf.invoice_receipt_id AS invoice_receipt_id
+    pi.invoice_receipt_id AS invoice_receipt_id
 FROM 
     in_invoice_processing inp
 JOIN LATERAL
@@ -19,12 +31,7 @@ JOIN LATERAL
     jsonb_array_elements(lig.value -> 'LineItems') WITH ORDINALITY AS li(value, idx) ON true
 JOIN LATERAL
     jsonb_array_elements(li.value -> 'LineItemExpenseFields') li_expense_fields(value) ON true
-JOIN LATERAL (
-        SELECT 
-            MAX(CASE WHEN summary_field -> 'Type' ->> 'Text' = 'INVOICE_RECEIPT_ID' THEN summary_field -> 'ValueDetection' ->> 'Text' END) AS invoice_receipt_id
-        FROM 
-            jsonb_array_elements(ed.value -> 'SummaryFields') AS summary_field
-    ) sf ON true
+JOIN preferred_invoice_id pi ON inp.id = pi.in_invoice_processing_id
 ORDER BY inp.id, ed.idx, li.idx;
 
 COMMENT ON VIEW ext1_line_item IS $$
@@ -33,3 +40,4 @@ This view 'ext1_line_item' is designed to extract and transform detailed line it
 1. **Extraction of Basic Invoice Data**: It retrieves the invoice processing ID, S3 object key, and timestamp from the 'in_invoice_processing' table, ensuring each entry is uniquely identifiable.
 
 2. **Line Item Decomposition**: Utilizes LATERAL joins to navigate through nested JSON structures from Textract outputs. It breaks down 'LineItemGroups' into individual 'LineItems' and extracts associated 'LineItemExpenseFields'.
+$$;

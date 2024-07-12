ae17358be8b4a02bb3f113f6c8366f7f07cc5a83
CTII <clay.thompson13@gmail.com>
2024-07-11 23:26:46 -0400

    ✨🐍 Add gl_level_3_by_gl3 view to fetch GL Level 3 data based on GL LVLs
    - Introduce new view gl_level_3_by_gl3 to retrieve GL Level 3 data filtered by GL Level 1, GL Level 2, and GL Level 3 IDs.
    - The view fetches and returns GL Level 3 details, including names and IDs, from the ConsolidatedGL model.
    - Return an error message if any of the required IDs (GL Level 1, GL Level 2, or GL Level 3) are not provided.

--- invoice/views.py
+++ invoice/views.py
@@ -198,6 +198,17 @@ def gl_level_3_by_gl2(request):
     gl1_id = request.GET.get('gl1_id')
     gl2_id = request.GET.get('gl2_id')
     if gl1_id and gl2_id:
-        gl3_items = ConsolidatedGL.objects.filter(gl_level_1_id=gl1_id, gl_level_2_id=gl2_id).values('gl_level_1_name', 'gl_level_2_name', 'gl_level_3_name')
+        gl3_items = ConsolidatedGL.objects.filter(gl_level_1_id=gl1_id, gl_level_2_id=gl2_id).values('gl_level_1_name', 'gl_level_2_name', 'gl_level_3_name', 'gl_level_3_id')
         return JsonResponse(list(gl3_items), safe=False)
     return JsonResponse({"error": "GL Level 1 ID or GL Level 2 ID not provided"}, status=400)
+
+@login_required(login_url='loginPage')
+def gl_level_3_by_gl3(request):
+    gl1_id = request.GET.get('gl1_id')
+    gl2_id = request.GET.get('gl2_id')
+    gl3_id = request.GET.get('gl3_id')
+    if gl1_id and gl2_id and gl3_id:
+        gl3_items = ConsolidatedGL.objects.filter(gl_level_1_id=gl1_id, gl_level_2_id=gl2_id, gl_level_3_id=gl3_id).values('gl_level_1_name', 'gl_level_2_name', 'gl_level_3_name', 'gl_level_3_id')
+        return JsonResponse(list(gl3_items), safe=False)
+    return JsonResponse({"error": "GL Level 1 ID, GL Level 2 ID, or GL Level 3 ID not provided"}, status=400)
+

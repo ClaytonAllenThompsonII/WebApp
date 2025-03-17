a172cf30326589d69843f92c86cf3078c2c76758
CTII <clay.thompson13@gmail.com>
2025-03-17 02:48:02 -0400

    🔧🐍 Remove explicit auth settings from ProductClassificationViewSet
    - Removed authentication_classes and permission_classes from ProductClassificationViewSet as they are now handled globally in settings.py.
    - Default authentication is set to TokenAuthentication, and permissions default to IsAuthenticated.
    - Temporarily set permission_classes = [AllowAny] for testing purposes.

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 # api/views.py
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.authentication import TokenAuthentication
 
 from invoice.models import ProductClassification
@@ -13,10 +13,12 @@ class ProductClassificationViewSet(viewsets.ModelViewSet):
     queryset = ProductClassification.objects.all()
     serializer_class = ProductClassificationSerializer
     
+
+    # Removing auth; handled in settings.py
     # Option 1: If you didn't set the default auth in settings.py:
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    #authentication_classes = [TokenAuthentication]
+    #permission_classes = [IsAuthenticated]
 
     # Option 2: If you set the default TokenAuthentication in settings.py,
-    # you only need:
-    # permission_classes = [IsAuthenticated]
+    # you only need: This is for testing
+    #permission_classes = [AllowAny]
